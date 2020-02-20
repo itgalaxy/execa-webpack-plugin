9e95af9186e0dcf3d81c86b26ef3b2650cc4f8bb
evilebottnawi <sheo13666q@gmail.com>
2020-02-20 14:34:47 +0300

    ci: github actions

--- __tests__/helpers/normalize-errors.js
+++ __tests__/helpers/normalize-errors.js
@@ -4,16 +4,16 @@ function removeCWD(str) {
   const isWin = process.platform === "win32";
   let cwd = process.cwd();
 
+  console.log(str);
+  console.log(process.cwd());
+  console.log(cwd);
+  console.log(isWin);
+
   if (isWin) {
     // eslint-disable-next-line no-param-reassign,
     str = str.replaceAll("\\", "/");
     cwd = cwd.replaceAll("\\", "/");
   }
-
-  console.log(str);
-  console.log(process.cwd());
-  console.log(cwd);
-  console.log(isWin);
 
   return str.replace(new RegExp(cwd, "g"), "");
 }
