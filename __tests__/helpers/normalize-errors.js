607dd6864e75b947a9b462bfa6675a96a4eba698
evilebottnawi <sheo13666q@gmail.com>
2020-02-21 19:19:52 +0300

    ci: github actions

--- __tests__/helpers/normalize-errors.js
+++ __tests__/helpers/normalize-errors.js
@@ -4,7 +4,7 @@ function removeCWD(str) {
   const isWin = process.platform === "win32";
   let cwd = process.cwd();
 
-  if (/ENOENT/.test(str)) {
+  if (/(ENOENT|exit code 1: not-found)/.test(str)) {
     // eslint-disable-next-line no-param-reassign
     str = str.replace(/return .+/, 'return "ENOENT"');
   }
